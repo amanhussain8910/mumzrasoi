ae349605cf77796679f69f0e690f1a0bb89a8d02
Aman Hussain <v-hussaina@microsoft.com>
2023-03-10 03:21:37 +0530

    changes

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(window).scroll(function(){
+    if($(window).scrollTop() > 60) {
+        $("header").addClass("active");
+    }else{
+        $("header").removeClass("active");
+    }
+})
+
+$(document).ready(function(){
+    $('.menu-dish-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<div class="slide-arrow prev-arrow"><img src="images/icons/left-arrow.png" alt=""></div>',
+        nextArrow: '<div class="slide-arrow next-arrow"><img src="images/icons/right-arrow.png" alt=""></button>',
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+        ]
+    });
+})
